e2176adb22b9ce527cc822287d149d3ff5cc4594
Lee Jae Jun <linho301150@gmail.com>
2022-08-21 18:55:46 +0900

    Feat: Jinja2Templates 및 login page 추가

--- authentication/controller/authentication_controller.py
+++ authentication/controller/authentication_controller.py
@@ -1,12 +1,20 @@
-from fastapi import APIRouter
+from authentication.service.authentication_service import AuthenticationService
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
-from ..service.authentication_service import AuthenticationService
 
 router = APIRouter(prefix="/auth",tags=["auth"],responses={404:{"description":"Not found"}})
+
+templates = Jinja2Templates(directory="templates")
 
 class SignData(BaseModel):
   email:str
   password:str
+
+@router.get("/", response_class=HTMLResponse)
+def render_loginPage(request:Request):
+  return templates.TemplateResponse("login.html", {"request": request})
 
 @router.get("/id/{id}/pwd/{pwd}")
 async def signIn(email:str,password:str):
