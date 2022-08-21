9d4dc3805826f15954f1284df21b49860c206d48
Lee Jae Jun <linho301150@gmail.com>
2022-08-21 18:55:16 +0900

    Feat: Jinja2Templates 및 index page 추가

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 import authentication.controller.authentication_controller as authentication_controller
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.include_router(authentication_controller.router)
+@app.get("/")
+async def render_indexPage(request:Request):
+    return templates.TemplateResponse("index.html",{"request":request})
+
+
