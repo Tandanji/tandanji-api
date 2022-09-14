c73918e8e82808fcec9bee77bb8f4a0e520c3e2f
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2022-09-14 14:47:35 +0900

    Feat: 권장량과 섭취량을 비교하여 퍼센트 구하는 로직추가

--- taking_picture/taking_picture.py
+++ taking_picture/taking_picture.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form, File
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import UploadFile
 from pydantic import BaseModel
 from model.detect import FoodModel
-
+import numpy as np
 import string
 import random
 import os
@@ -14,12 +14,13 @@ router = APIRouter(prefix="/taking-pictures",tags=["pictures"],responses={404:{"
 
 templates = Jinja2Templates(directory="templates")
 
+
 @router.get("/", response_class=HTMLResponse)
 def render_takingPicturePage(request:Request):
   return templates.TemplateResponse("foodguide.html", {"request": request})
 
 @router.post("/uploadfile")
-async def create_upload_file(file: UploadFile, request:Request):
+async def create_upload_file(file: UploadFile=File(...), gender:str=Form(...),age:int=Form(...)):
   folder_location = os.path.join(f"upload",id_generator())
   os.mkdir(folder_location)
   file_location = os.path.join(folder_location,file.filename)
@@ -27,9 +28,16 @@ async def create_upload_file(file: UploadFile, request:Request):
     file_object.write(file.file.read())
   strFoods=await activeModel(folder_location)
   food,cal=getFoodsByCSV(strFoods)
+  recommendNutrient=getNutrientByCSV(gender,age)
+  currentPercent=calcPercent(recommendNutrient,cal)
+  #str(food)
+  print(cal)
+  print(currentPercent[-1])
+
   '''url = f'{request.client.host}/?{params}'
   response = RedirectResponse(url=url)'''
-  return {"predict": str(food)+" "+str(cal)}
+
+  return {"predict": str(currentPercent)}
 
 def id_generator(size=20, chars=string.ascii_uppercase + string.digits):
    return ''.join(random.choice(chars) for _ in range(size))
@@ -42,12 +50,33 @@ def getFoodsByCSV(strFoods):
   foodlist=strFoods.split()
   dataFrame=pd.read_csv("model/caldata/caldata.csv")
   foodData=[]
-  calData=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  calData=[0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   for index in foodlist:
     data=list(dataFrame[dataFrame["food"]==index].iloc[0])
     foodData.append(data[0])
-    calData=[x+y for x,y in zip(calData, data[1:])]
+    calData=[x+y for x,y in zip(calData, data[2:])]
   return [foodData,calData]
+
+def getNutrientForGender(gender):
+  filePath="model/nutrient-data/man_nutrient.csv"
+  if(gender=="male"):
+    filePath="model/nutrient-data/woman_nutrient.csv"
+    return filePath
+
+def getNutrientByCSV(gender,age):
+  filePath=getNutrientForGender(gender)
+  nutrientData=pd.read_csv(filePath)
+  nutrientList=list(nutrientData[(nutrientData["start"] < age) & (age <= nutrientData["end"])].iloc[0])
+  return nutrientList[2:]
+
+def calcPercent(recommendNutrientList, currentNutrientList):
+  data=[round((recommend/current)*100,2) for recommend,current in zip(recommendNutrientList, currentNutrientList)]
+  return np.nan_to_num(data,copy=True,posinf=0)
+
+  
+
+  
+
 
 
 #음 식 명,중량(g),에너지(kcal),탄수화물(g),당류(g),지방(g),단백질(g),칼슘(mg),인(mg),나트륨(mg),칼륨(mg),마그네슘(mg),철(mg),아연(mg),콜레스테롤(mg),트랜스지방(g)
